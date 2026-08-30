f69555f86e8c0bf4015d046d33ea72040b50c249
Shreya Singh <shreyasinghin24@gmail.com>
2026-08-30 17:38:32 +0530

    c

--- lib/email/resend.ts
+++ lib/email/resend.ts
@@ -1,10 +1,10 @@
 import { Resend } from 'resend'
 
-const resend = new Resend(process.env.RESEND_API_KEY)
 const FROM = process.env.RESEND_FROM_EMAIL ?? 'onboarding@resend.dev'
 
 export async function sendEmail(to: string, subject: string, html: string) {
   try {
+    const resend = new Resend(process.env.RESEND_API_KEY)
     await resend.emails.send({ from: FROM, to, subject, html })
   } catch (error) {
     console.error('Email send failed', { to, subject, error })
